eb6d942391d4631a3311dccc853084f83f503f9e
Lex Chudzik <chudzik.alex@gmail.com>
2018-11-04 16:04:07 -0600

    data in table

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -11,10 +11,12 @@ class Admin extends Component {
     }
 
     getFeedback = () => {
+        console.log('getting feedback');
+        
         axios.get('/feedback')
         .then((response) => {
             console.log('The orders have arrived from the GET request!', response.data);
-            
+            this.props.dispatch({ type: 'GET_FEEDBACK', payload: response.data})
         }).catch((error) => {
             alert('Unable to GET feedback!');
         })
@@ -22,11 +24,28 @@ class Admin extends Component {
 
     render() {
     return (
-      <ul>
-        {/* { this.props.reduxState.displayReducer.map(
-            feedback => <li key={}></li>
-        )} */}
-      </ul>
+      <table>
+          <thead>
+                <tr>
+                    <th>Feeling</th>
+                    <th>Comprension</th>
+                    <th>Support</th>
+                    <th>Comments</th>
+                    <th>Delete</th>
+                </tr>
+          </thead>
+          <tbody>
+        { this.props.reduxState.displayReducer.map(
+            feedback => <tr key={feedback.id}>
+                <td>{feedback.feeling}</td>
+                <td>{feedback.understanding}</td>
+                <td>{feedback.support}</td>
+                <td>{feedback.comments}</td>
+                <td>DELETE button here</td>
+            </tr>
+        )}
+        </tbody>
+      </table>
         )
     }
 }
